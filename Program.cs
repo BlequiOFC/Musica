91b4a4909c90572b0085b1cfe701a7c8316951eb
Gustavo <gustavopregigueiro6@gmail.com>
2023-03-17 21:25:03 -0300

    resultado

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿Console.Clear();
+Console.WriteLine("Happier than ever - Billie Eilish");
+Thread.Sleep(4000);
+
+Console.WriteLine("");
+
+Console.WriteLine("When I'm away from you, I'm happier than ever");
+Console.WriteLine("Wish I could explain it better");
+Console.WriteLine("I wish it wasn't true");
+Console.WriteLine("Give me a day or two to think of something clever");
+Console.WriteLine("To write myself a letter");
+Console.WriteLine("To tell me what to do, mm-mmm");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("Do you read my interviews?");
+Console.WriteLine("Or do you skip my avenue? (My avenue)");
+Console.WriteLine("When you (when you) said you were passing through");
+Console.WriteLine("Was I even on your way?");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("I knew when I asked you to (when I asked you to)");
+Console.WriteLine("Be cool about what I was telling you");
+Console.WriteLine("You'd do the opposite of what you said you'd do (what you said you'd do)");
+Console.WriteLine("And I'd end up more afraid");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("Don't say it isn't fair");
+Console.WriteLine("You clearly weren't aware that you made me miserable");
+Console.WriteLine("So if you really wanna know");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("When I'm away from you (when I'm away from you)");
+Console.WriteLine("I'm happier than ever (happier than ever)");
+Console.WriteLine("Wish I could explain it better (wish I could explain it better)");
+Console.WriteLine("I wish it wasn't true (wish it wasn't true), mmm-mmm");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("You call me again, drunk in your Benz");
+Console.WriteLine("Driving home under the influence");
+Console.WriteLine("You scared me to death, but I'm wasting my breath");
+Console.WriteLine("'Cause you only listen to your fucking friends");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("I don't relate to you");
+Console.WriteLine("I don't relate to you, no");
+Console.WriteLine("'Cause I'd never treat me this shitty");
+Console.WriteLine("You made me hate this city");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("And I don't talk shit about you on the internet");
+Console.WriteLine("Never told anyone anything bad");
+Console.WriteLine("'Cause that shit's embarrassing, you were my everything");
+Console.WriteLine("And all that you did was make me fucking sad");
+Console.WriteLine("So don't waste the time I don't have");
+Console.WriteLine("And don't try to make me feel bad");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("I could talk about every time that you showed up on time");
+Console.WriteLine("But I'd have an empty line 'cause you never did");
+Console.WriteLine("Never paid any mind to my mother or friends");
+Console.WriteLine("So I shut 'em all out for you 'cause I was a kid");
+
+Thread.Sleep(2000);
+Console.WriteLine("");
+
+Console.WriteLine("You ruined everything good");
+Console.WriteLine("Always said you were misunderstood");
+Console.WriteLine("Made all my moments your own");
+Console.WriteLine("Just fucking leave me alone");
